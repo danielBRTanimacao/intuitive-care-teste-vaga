41fe88dd7e43f1d231ac6ee219feec88c685affc
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-24 18:44:55 -0300

    feat: created main add pdfs and athchements final created code with compression

--- test-web-scraping/main.py
+++ test-web-scraping/main.py
@@ -1,2 +1,15 @@
+from zipfile import ZipFile
+from pathlib import Path
+
 if __name__ == "__main__":
-    print("aoo")
+    PDFS_PATH = Path() / "pdfs"
+    ZIP_PATH = Path() / "zipFiles"
+    ZIP_PATH_NAME = ZIP_PATH / "pdfs.zip"
+
+    ZIP_PATH.mkdir(exist_ok=True)
+
+    with ZipFile(ZIP_PATH_NAME, "w") as myzip:
+        for pdf in PDFS_PATH.glob("*.pdf"):
+            myzip.write(pdf, pdf.name)
+
+    print(PDFS_PATH.absolute())
