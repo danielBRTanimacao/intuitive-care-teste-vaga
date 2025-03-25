eb76fc176fa60e0e7aaac76d2fab9955198fe63a
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-25 10:08:17 -0300

    feat build: add new requirements using tabela-py to request data on pdf trying new logical to collect and sabe csv

--- test-transform-data/main.py
+++ test-transform-data/main.py
@@ -1,9 +1,27 @@
 from pathlib import Path
-from pypdf import PdfReader
+from zipfile import ZipFile
+import csv
+import tabula
+
+PDF = Path() / "pdfs" / "Anexo1.pdf"
+DIRECTORY_CSV = Path() / "csvs"
+CSV_FILE = DIRECTORY_CSV / "rol-procedimentos-em-saude.csv"
+DIRECTORY_ZIP = Path() / "zipFiles"
+
+your_name = "Daniel"  # Pode alterar o nome
+ZIP_NAME = DIRECTORY_ZIP / f"Teste_{your_name}.zip"
+
+abbreviations = {
+    'OD': 'Seg. Odontológica',
+    'AMB': 'Seg. Ambulatorial'
+}
+
+DIRECTORY_CSV.mkdir(exist_ok=True)
+DIRECTORY_ZIP.mkdir(exist_ok=True)
 
 if __name__ == "__main__":
-    PDF = Path() / "pdfs" / "Anexo1.pdf"
-    ZIP_PATH = Path() / 
+    TABLES_PDF = tabula.read_pdf(PDF, pages='3')
 
-    pdf_reader = PdfReader(PDF)
-    print(pdf_reader.get_num_pages())
+    for table in TABLES_PDF:
+        print(table)
+        
